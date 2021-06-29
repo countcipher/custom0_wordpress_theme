2d9a9f2b9e59516020448c673d3450966657d98a
Count Cipher <chrisharlow366@yahoo.com>
2021-06-29 18:34:33 -0400

    com

--- homepage-template.php
+++ homepage-template.php
@@ -8,9 +8,9 @@ Template Name: Homepage
 
 <?php get_header(); ?>
     <section class="hero">
-        <h1><?php echo get_theme_mod('banner_heading'); ?></h1>
+        <h1><?php echo get_theme_mod('banner_heading', 'Banner Heading'); ?></h1>
 
-        <h2><?php echo get_theme_mod('banner_tagline'); ?></h2>
+        <h2><?php echo get_theme_mod('banner_tagline', 'Tagline goes here'); ?></h2>
 
         <button>Click Me!</button>
     </section>
@@ -42,12 +42,16 @@ Template Name: Homepage
     </section>
 
     <section class="main-content">
-        <div class="image">
+    <div class="front-page-content">
+        <?php the_content(); ?>
+    </div>
+    
+        <!-- <div class="image">
             <img src="https://cdn.pixabay.com/photo/2016/01/19/14/56/office-work-1149087_960_720.jpg" alt="">
-        </div>
+        </div> -->
         
 
-        <div class="text">
+        <!-- <div class="text">
             <h1>Title</h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sapiente ducimus vero dolorum, velit at quos molestiae excepturi a repudiandae eaque consequuntur sunt animi quisquam.</p>
@@ -57,7 +61,7 @@ Template Name: Homepage
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis sapiente ducimus vero dolorum, velit at quos molestiae excepturi a repudiandae eaque consequuntur sunt animi quisquam.</p>
 
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, magnam? Esse accusantium unde reprehenderit fugiat consectetur sint aliquam doloribus non ipsum atque alias placeat nemo, nam iste libero, ullam dignissimos. Corporis mollitia dicta atque dolore quod quas cumque ullam neque ipsum. Cumque odit repellat et.</p>
-        </div>
+        </div> -->
     </section>
 
     <?php get_footer(); ?>
